633901b815bc24e9aef8ef23ec4c54364234c1f9
kenm6 <kenm661@gmail.com>
2023-06-14 03:53:04 -0500

    request body add fix

--- src/main/java/com/catalog/controllers/RecordingController.java
+++ src/main/java/com/catalog/controllers/RecordingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.catalog.entities.Recording;
@@ -22,7 +23,7 @@ public class RecordingController {
 	}
 
 	@PostMapping("/api/save-recording")
-	public String saveRecording(Recording recording) {
+	public String saveRecording(@RequestBody Recording recording) {
 		service.addRecording(recording);
 		return recording.toString();
 	}
