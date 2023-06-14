0f6c91f9eb7067184343f789cf0acd71c2edbca3
kenm6 <kenm661@gmail.com>
2023-06-13 20:35:51 -0500

    first

---
+++ src/main/java/com/catalog/controllers/CatalogController.java
@@ -0,0 +1,15 @@
+package com.catalog.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CatalogController {
+	
+	
+	@GetMapping("/demo-string")
+	public String demo(){
+		return "This is the demo";
+	}
+
+}
---
+++ src/main/java/com/catalog/entities/Recording.java
@@ -0,0 +1,5 @@
+package com.catalog.entities;
+
+public class Recording {
+
+}
