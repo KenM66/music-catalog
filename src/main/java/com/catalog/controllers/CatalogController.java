66d4c7f5da30fa8b221e41dd0731d56967569760
KenM66 <kenm661@gmail.com>
2023-07-15 04:18:05 -0500

    Object updated.

--- src/main/java/com/catalog/controllers/CatalogController.java
+++ src/main/java/com/catalog/controllers/CatalogController.java
@@ -2,6 +2,8 @@ package com.catalog.controllers;
 
 import com.catalog.entities.SongRequest;
 import com.catalog.services.EmailService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +14,20 @@ public class CatalogController {
 	
 	@Autowired
 	private EmailService service;
+	@Autowired
+	ObjectMapper mapper;
 	@GetMapping("/demo-string")
 	public String demo(){
 		return "Hello Ken's Facebook Friends";
 	}
 
 	@PostMapping("/send-message")
-	public String sendMessage(@RequestBody SongRequest request){
+	public String sendMessage(@RequestBody SongRequest request) throws JsonProcessingException {
 
 		System.out.println(request.getMessage());
 
 
 		service.sendRequestEmail(request);
-		return "Your song request has been sent to Ken Milota!";
+		return mapper.writeValueAsString("Your song request has been sent to Ken Milota!");
 	}
 }
